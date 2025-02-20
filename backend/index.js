011370339fc8167484f38f98602b6922fa921622
Shakti <shaktisaxena2002@gmail.com>
2025-02-20 20:01:42 +0530

    Completed

--- backend/index.js
+++ backend/index.js
@@ -41,9 +41,7 @@ app.use(express.json());
 app.use(authRouter);
 app.use(userRouter);
 app.use(messageRouter);
-app.use("/", (req, res) => {
-  res.send("Hello from Backend!!!!!!!!");
-});
+
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(join(__dirname, "../frontend/dist")));
