8220cfb3b942e0ae9e46c4e35d9993d55c5b3eea
Shakti <shaktisaxena2002@gmail.com>
2025-02-20 15:17:48 +0530

    Done

--- backend/index.js
+++ backend/index.js
@@ -10,13 +10,23 @@ const { userRouter } = require("./src/routes/user");
 const { messageRouter } = require("./src/routes/message");
 const { app, server } = require("./src/middlewares/socket");
 
+const allowedOrigins = [process.env.FRONTEND_URL]; // List of allowed origins
+
+// CORS middleware configuration
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: function (origin, callback) {
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     allowedHeaders: ["Content-Type"],
-    credentials: true,
+    credentials: true, // if you need cookies to be sent along with requests
   })
 );
+
 app.options("*", cors());
 app.use(cookieParser());
 app.use(express.json());
@@ -24,9 +34,9 @@ app.use(express.json());
 app.use(authRouter);
 app.use(userRouter);
 app.use(messageRouter);
-app.use("/",(req,res)=>{
-  res.send("Hello from Backend!!!!!!!!")
-})
+app.use("/", (req, res) => {
+  res.send("Hello from Backend!!!!!!!!");
+});
 
 connectDB()
   .then(() => {
