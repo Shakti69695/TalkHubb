d29f9b5b773164de5197fca736eda11cd9363945
Shakti <shaktisaxena2002@gmail.com>
2025-02-21 10:22:41 +0530

    Comp

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 import cookieParser from "cookie-parser";
-const PORT = process.env.PORT|| 3000;
+const PORT = process.env.PORT || 3000;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 import connectDB from "./src/config/database.js";
 import { authRouter } from "./src/routes/auth.js";
@@ -20,16 +20,16 @@ app.use(
     credentials: true, // Allow credentials (cookies) to be sent
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"], // Allowed methods
     allowedHeaders: [
-      "Content-Type", 
-      "Authorization", 
-      "Cache-Control", 
-      "X-Requested-With", 
-      "Accept", 
-      "Accept-Version", 
-      "Content-Length", 
-      "Content-MD5", 
-      "Date", 
-      "X-Api-Version"
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "X-Requested-With",
+      "Accept",
+      "Accept-Version",
+      "Content-Length",
+      "Content-MD5",
+      "Date",
+      "X-Api-Version",
     ],
   })
 );
@@ -41,7 +41,9 @@ app.use(express.json());
 app.use(authRouter);
 app.use(userRouter);
 app.use(messageRouter);
-
+app.use("/ping", (req, res) => {
+  res.send("pong");
+});
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(join(__dirname, "../frontend/dist")));
